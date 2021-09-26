040c85d2cec11b04e799c24270839b8f4cecb252
Lucas Miranda <lucasrezende12@hotmail.com>
2021-09-26 12:06:08 -0300

    hw 3

---
+++ Works/3 - Primitive Operations Analysis/app.c
@@ -0,0 +1,36 @@
+//Atividade Pratica 3
+//Nome:       Lucas Miranda
+//Numero USP: 12542838
+//Professor:  Renato Tinós
+
+//Todos os arquivos estão disponíveis no github: https://github.com/miiranta/ic2-all
+
+#include <stdio.h>
+#define N 10
+
+int findX(int* v, int n, int x);
+
+void main(){
+
+    //Search word
+    int x = 23;
+
+    //Vector
+    int v[N] = {1,2,3,4,5,6,7,8,23};
+
+    //Print
+    printf("%d", findX(v, N, x));
+
+}
+
+int findX(int* v, int n, int x){
+    int i = 0;
+
+    for(i = 0; i<n; i++){
+        if(v[i] == x){
+            return v[i];
+        }
+    }
+    return -1;
+
+}
